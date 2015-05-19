e112bf6e3b02419cbbba4134f449d48b31c59b10
migrodcab <miguel.rc95@gmail.com>
2015-05-19 20:32:11 +0200

    Prueba

--- index.php
+++ index.php
@@ -23,6 +23,7 @@
 		<div id="presentacion">
 		<h1>Breve Resumen</h1>
 		<h2>Migue guarra</h2>
+		<h1>Puton</h1>
 		?¿?¿?¿?¿
 		Para más información consultar: <a href="/acercade.php"> acerca de</a>.
 		<h2>Otra info interesante</h2>
