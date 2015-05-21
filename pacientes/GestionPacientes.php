5322994ed16a5733815b72ec7813dca60d327a4e
Manuel López Ruiz <manloprui1@alum.us.es>
2015-05-21 11:29:09 +0200

    Some PHP code

--- pacientes/GestionPacientes.php
+++ pacientes/GestionPacientes.php
@@ -1,36 +1,40 @@
 <?php
-	//basarse en la práctica 6 -> GestionarEntradas.php
-	function insertarPaciente($nombre, $apellidos, $nuhsa, $nhc, $diagnostico, $medicacion, $fechaInclusion, $idEnsayoClinico, $conexion){
-		$result=true;
-		try{
-			$stmt = $conexion->prepare("CALL CREA_PACIENTE(:NOMBRE,
+//basarse en la práctica 6 -> GestionarEntradas.php
+function insertarPaciente($nombre, $apellidos, $nuhsa, $nhc, $diagnostico, $medicacion, $fechaInclusion, $idEnsayoClinico, $conexion) {
+	$result = true;
+	try {
+		$stmt = $conexion -> prepare("CALL CREA_PACIENTE(:NOMBRE,
 			 :APELLIDOS, :NUHSA, :NHC, :DIAGNOSTICO, :MEDICACION_AUX,
 			 to_date(:FECHA_INCLUSION,'yyyy-mm-dd'), :ID_EC)");
-			$stmt->bindParam(':NOMBRE',$nombre);
-			$stmt->bindParam(':APELLIDOS',$apellidos);
-			$stmt->bindParam(':NUHSA',$nuhsa);
-			$stmt->bindParam(':NHC',$nhc);
-			$stmt->bindParam(':DIAGNOSTICO',$diagnostico);
-			$stmt->bindParam(':MEDICACION_AUX',$medicacion);
-			$stmt->bindParam(':FECHA_INCLUSION',$fechaInclusion);
-			$stmt->bindParam(':ID_EC',$idEnsayoClinico);
-			
-			$stmt->execute();
-		}catch(PDOException $e){
-			//Tratamiento del error
-			$result=false;
-			echo "<div id='muestraErrores'>";
-			echo "<div class='error'>";
-			echo "<b>ERROR: </b>" . $e->GetMessage();
-			echo "</div>";
-			echo "</div>";
-		}
-		return $result;
+		$stmt -> bindParam(':NOMBRE', $nombre);
+		$stmt -> bindParam(':APELLIDOS', $apellidos);
+		$stmt -> bindParam(':NUHSA', $nuhsa);
+		$stmt -> bindParam(':NHC', $nhc);
+		$stmt -> bindParam(':DIAGNOSTICO', $diagnostico);
+		$stmt -> bindParam(':MEDICACION_AUX', $medicacion);
+		$stmt -> bindParam(':FECHA_INCLUSION', $fechaInclusion);
+		$stmt -> bindParam(':ID_EC', $idEnsayoClinico);
+
+		$stmt -> execute();
+	} catch(PDOException $e) {
+		//Tratamiento del error
+		$result = false;
+		echo "<div id='muestraErrores'>";
+		echo "<div class='error'>";
+		echo "<b>ERROR: </b>" . $e -> GetMessage();
+		echo "</div>";
+		echo "</div>";
 	}
-	function seleccionarPacientes($conexion){
-  	$SQL = "SELECT * FROM PACIENTE";
+	return $result;
+}
+
+function seleccionarPacientes($conexion) {
+	$SQL = "SELECT * FROM PACIENTE";
 	$stmt = $conexion -> query($SQL);
 	return $stmt;
-  }
+}
 
+function modificarPaciente($conexion, $OidPaciente, $nombre, $apellidos, $nuhsa, $nhc, $diagnostico, $medicacion, $fechaInclusion, $idEnsayoClinico) {
+
+}
 ?>
