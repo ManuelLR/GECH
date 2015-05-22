3a36122a3c6de1a097a9a1613c093a2f8ed27658
Manuel López Ruiz <manloprui1@alum.us.es>
2015-05-22 23:21:44 +0200

    A diferent design for the foot

--- Pie.php
+++ Pie.php
@@ -2,7 +2,7 @@
 			<div class="navegacionPie">
 				<ul><li class="pestanaNavPie primPestanaNavPie"><a title="Mapa" href="/mapa.php">Mapa de la web</a></li>
 			 	<li class="pestanaNavPie"><a title="Contacto" href="mailto: manloprui1@alum.us.es">Contacto</a></li>
-			 	<li class="pestanaNavPie"><a title="Acerca de" href="/acercade.php">Acerca de</a></li></ul>
+			 	<li class="pestanaNavPie"><a title="Acerca de" href="/acercade.php">Acerca de</a></li>
 			 	<li class="pestanaNavPie"><a title="Código SQL" href="/codigosql.php">Código SQL</a></li></ul>
 			</div>
 			<img src="/images/LogoCabecera3.png" alt="GECH" width="46"><br/>
