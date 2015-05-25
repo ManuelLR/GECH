29e2eff4a08b28ceb811a1728a9acb2cb1c806cb
migrodcab <miguel.rc95@gmail.com>
2015-05-25 19:08:29 +0200

    cambios en pendiente

--- Pie.php
+++ Pie.php
@@ -1,12 +1,23 @@
-		<div id="pie">
-			<div class="navegacionPie">
-				<ul>
-					<li class="pestanaNavPie primPestanaNavPie"><a title="Mapa" href="/mapa.php">Mapa de la web</a></li>
-			 		<li class="pestanaNavPie"><a title="Contacto" href="mailto: manloprui1@alum.us.es">Contacto</a></li>
-			 		<li class="pestanaNavPie"><a title="Acerca de" href="/acercade.php">Acerca de</a></li>
-			 		<li class="pestanaNavPie"><a title="Código SQL" href="/TodoSQL.php">Código SQL</a></li>
-			 	</ul>
-			</div>
-			<img src="/images/LogoCabecera3.png" alt="GECH" width="46"><br/>
-			GECH. 2015. Manuel López y Miguel Rodríguez.
-		</div>
+<div id="pie">
+	<div class="navegacionPie">
+		<ul>
+			<li class="pestanaNavPie primPestanaNavPie">
+				<a title="Mapa" href="/mapa.php">Mapa de la web</a>
+			</li>
+			<li class="pestanaNavPie">
+				<a title="Acerca de" href="/acercade.php">Acerca de</a>
+			</li>
+			<li class="pestanaNavPie">
+				<a title="Código SQL" href="/TodoSQL.php">Código SQL</a>
+			</li>
+		</ul>
+	</div>
+	<img src="/images/LogoCabecera3.png" alt="GECH" width="46">
+	<br/>
+	GECH. 2015. Manuel López y Miguel Rodríguez.
+	<p>
+		<a href="http://jigsaw.w3.org/css-validator/check/referer"> <img style="border:0;width:88px;height:31px"
+		src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+		alt="¡CSS Válido!" /> </a>
+	</p>
+</div>
