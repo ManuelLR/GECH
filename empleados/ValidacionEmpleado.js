6c38a1e07ca99d87142c4cd89f90cf71aa63761f
migrodcab <miguel.rc95@gmail.com>
2015-06-19 12:25:28 +0200

    Valida dni y telefono js

--- empleados/ValidacionEmpleado.js
+++ empleados/ValidacionEmpleado.js
@@ -6,7 +6,7 @@ function validaForm() {
 	document.getElementById("errores").innerHTML="";
 	
 	if(compruebaVacio("nombre") || compruebaVacio("apellidos") || 
-	compruebaVacio("dni") || compruebaVacio("telefono") || compruebaVacio("email")) {
+	compruebaVacio("dni") || compruebaVacio("telefono") || compruebaVacio("email") || !nif("dni") || !telefono("telefono")) {
 		document.getElementById("errores").innerHTML+="<div id ='muestraErrores'><\div>";
 		res = false;
 	}
@@ -31,12 +31,18 @@ function validaForm() {
   	if(compruebaVacio("dni")){
   		document.getElementById("label_dni").style.color = "red";
   		document.getElementById("muestraErrores").innerHTML+="El DNI no puede estar vacío<br/>";
+  	} else if(!nif("dni")) {
+  		document.getElementById("label_dni").style.color = "red";
+  		document.getElementById("muestraErrores").innerHTML+="El DNI no es válido<br/>";
   	} else {
   		document.getElementById("label_dni").style.color	= "black";
   	}
   	if(compruebaVacio("telefono")){
   		document.getElementById("label_telefono").style.color = "red";
   		document.getElementById("muestraErrores").innerHTML+="El teléfono no puede estar vacío<br/>";
+  	} else if (!telefono("telefono")) {
+  		document.getElementById("label_telefono").style.color = "red";
+  		document.getElementById("muestraErrores").innerHTML+="El teléfono no es válido<br/>";
   	} else {
   		document.getElementById("label_telefono").style.color	= "black";
   	}
@@ -63,4 +69,44 @@ function compruebaVacio(element) {
 }
 
 
+
+
+function nif(dniInput) {
+  var dni = document.getElementById(dniInput).value.toUpperCase();
+  var numero;
+  var leta;
+  var letra;
+  var expresion_regular_dni;
+  var res = true;
+ 
+  expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
+ 
+  if(expresion_regular_dni.test (dni) == true){
+     numero = dni.substr(0,dni.length-1);
+     numero = parseInt(numero);
+     leta = dni.substr(dni.length-1,1);
+     numero = numero % 23;
+     letra='TRWAGMYFPDXBNJZSQVHLCKET';
+     letra=letra.substring(numero,numero+1);
+    if (letra!=leta.toUpperCase()) {
+       res = false;
+     }
+  }else{
+     res = false;
+   }
+   return res;
+}
+
+
+
+
+function telefono(telInput) {
+	var res = true;
+	expresion_regular_telefono = /^\d{9}$/;
+	var telefono = document.getElementById(telInput).value;
+	if (!expresion_regular_telefono.test (telefono) == true) {
+		res = false;
+	}
+	return res;
+}
 
